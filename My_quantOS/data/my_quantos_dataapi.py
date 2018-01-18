fd58d8f736ed61929319d563b95643ee8ec5d61d
Scpb1026 <scpb1026@126.com>
2018-01-18 23:44:14 +0800

    init

--- My_quantOS/data/my_quantos_dataapi.py
+++ My_quantOS/data/my_quantos_dataapi.py
@@ -17,7 +17,6 @@ from jaqs.data.dataview import DataView
 
 class MyQuantosDataApi():
     def __init__(self):
-        self.universe = []
         self.symbol = ""
         self.fields = ""
         self.start_date = 0
@@ -34,6 +33,10 @@ class MyQuantosDataApi():
         self._remote_data_service = None
         self._instrument_info = pd.DataFrame()
         self._symbol_set_all_A = set()
+        self._dataview_data = pd.DataFrame()
+        self._dbase_props = {}
+        self._db_init_config = ['reference_daily_fields']
+        self._input_init_config = []
         
 
     def _login(self, dataapi=False, remote=False, mongodb=False):
@@ -63,18 +66,36 @@ class MyQuantosDataApi():
             self._col_list = self._db.collection_names()
 
 
-    def _prepare_data(self, first=False, update=False):
-        self._login(dataapi=True, mongodb=True)
+    def prepare_data(self, first=False, update=False):
+        self._get_fields()
+        self._login(dataapi=True, remote=True)
         self._get_instrumentInfo()
         self._get_symbol_set_all_A()
 
         if first:
+            self._db_name = self._fields_init_config
+            self._col_name = self.symbol
             self._get_start_end_date_first_time()
+
         if update:
+            self._login(mongodb=True)
             self._get_start_end_date_update()
-        pass
 
 
+    def _get_input_init_config(self):
+        "确定最终多线程map函数的输入列表，返回[(db_name, col_name)]"
+        for db_name in self._db_init_config:
+            for col_name in self._symbol_set_all_A:
+                self._input_init_config.append(db_name, col_name)
+
+
+    def _get_fields(self):
+        "确定查询字段，同时也确定mongoDB的db_name"
+        dv = DataView()
+        fields_init_config = {
+            'reference_daily_fields': dv.reference_daily_fields
+            # 此处可能增加新的字段，只要是qunatos的dataview支持的字段
+            }
 
 
     def _get_instrumentInfo(self):
@@ -134,6 +155,49 @@ class MyQuantosDataApi():
             self.end_date = int(today)
         else:
             print("Trade date in Dbase is latest.")
+
+
+
+
+    def _download_data(self):
+        "使用quantos的dataview，下载单个股票的给定字段的数据，返回pd.DataFrame"
+        dv = DataView()
+        # fields = ','.join(list(dv.reference_daily_fields))
+        props = {
+            'symbol': self.symbol,
+            'fields': self.fields,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'freq': 1
+            }
+
+        dv.init_from_config(props=props, data_api=self._remote_data_service)
+        dv.prepare_data()
+        self._dataview_data = dv.data_d
+
+
+    def _dataframe_to_dbase_props(self):
+        "将dataframe转换为mongoDB中的props，以备写入数据库"
+        df = self._dataview_date
+        df.columns = df.columns.droplevel()
+        df.reset_index(inplace=True)
+
+        # dataframe的范围限制在更新时期之内，因为dataview取数据时会将日期的范围前后各放宽几天
+        df = df[(df['trade_date'] >= self.start_date) & (df['trade_date'] <= self.end_date)]
+
+        # 判断drop掉trade_date列，并dropna后，dataframe是否为空，空则说明更新日期已经是最新的
+        is_df = df.drop('trade_date', axis=1).dropna(how='all')
+
+        if is_df.empty:
+            print("\n\nTrade date is latest.\n\n")
+            return None
+        else:
+            props = df.to_dict(orient='records')
+            self._dbase_props = props
+
+
+    def _write_data_to_dbase(self):
+        pass
 
 
 
